7838c431d5782e340c8c9ddf22bc113e69080e40
isher2k1 <ikekches@gmail.com>
2025-10-23 11:44:18 +0300

    Added logic to moveRobot()

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,43 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        final int xDestination = Math.abs(robot.getX() - toX);
+        final int yDestination = Math.abs(robot.getY() - toY);
+
+        if (toX <= robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            for (int i = 0; i < xDestination; i++) {
+                robot.stepForward();
+            }
+
+            if (toY < robot.getY()) {
+                robot.turnLeft();
+            } else {
+                robot.turnRight();
+            }
+
+            for (int i = 0; i < yDestination; i++) {
+                robot.stepForward();
+            }
+        } else if (toX >= robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < xDestination; i++) {
+                robot.stepForward();
+            }
+
+            if (toY < robot.getY()) {
+                robot.turnRight();
+            } else {
+                robot.turnLeft();
+            }
+
+            for (int i = 0; i < yDestination; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
